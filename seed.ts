77a6b7e088d9b31c84676765debef4aa5a95a637
Kyron Smith <kds060902@outlook.com>
2023-03-23 22:42:02 +0000

    refactor: add check to see if item already exists in dynamoDB

--- seed.ts
+++ seed.ts
@@ -1,36 +1,40 @@
-import { DynamoDBClient, PutItemCommand } from '@aws-sdk/client-dynamodb';
-import { marshall } from '@aws-sdk/util-dynamodb';
-import booksData from './books'; // Assuming the book data is stored in a JSON file named "books.json"
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import {
+  DynamoDBDocumentClient,
+  GetCommand,
+  PutCommand,
+} from "@aws-sdk/lib-dynamodb";
+import books from "./books";
 
-interface Book {
-  type: string;
-  category: string;
-  title: string;
-  author: string;
-  pageCount: number;
-  price: number;
-  publicationDate: string;
-  thumbnail: string;
-  stock: number;
-}
+const dbClient = new DynamoDBClient({ region: "eu-west-1" });
+const docClient = DynamoDBDocumentClient.from(dbClient);
 
-async function seedDatabase() {
-  const client = new DynamoDBClient({ region: 'eu-west-1' }); // Replace with your desired region
+const seedDb = async () => {
+  for (const book of books) {
+    console.log(book)
+    const checkExists = await docClient.send(
+      new GetCommand({
+        TableName: "alexandria-api",
+        Key: {
+          title: book.title,
+          category: book.category,
+        },
+      })
+    );
 
-  // Assuming the book data is stored in a JSON file named "books.json"
-  const bookData: Book[] = booksData;
-
-  // Iterate through the array of books and add them to the DynamoDB table
-  for (const book of bookData) {
-    const params = {
-      TableName: 'alexandria-api',
-      Item: marshall(book),
-    };
-
-    await client.send(new PutItemCommand(params));
+    if (checkExists.Item == null) {
+        console.log("You have attempted to write duplicate information into the DB, we have not put this duplicate data into the DB.")
+      await docClient.send(
+        new PutCommand({
+          TableName: "alexandria-api",
+          Item: book,
+        })
+      );
+    }
   }
-}
+};
 
-seedDatabase().catch((error) => {
-  console.error(error);
-});
+(async () => {
+  await seedDb();
+  console.log("Database has been seeded! :)")
+})();
